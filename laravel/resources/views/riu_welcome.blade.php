bd8a1a75c6966db4993209405bdd6a3370a52409
ricardo <ricardo.david.rd@gmail.com>
2019-02-13 15:12:41 -0500

    changed default page

---
+++ laravel/resources/views/riu_welcome.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <title> RiuComunnication Project</title>
+        <p> THis project ... </p>
+
+    </head>
+    <body>
+        
+    </body>
+</html>
